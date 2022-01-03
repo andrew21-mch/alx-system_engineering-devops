f517d726fa07f1fb04a6c4118c135764e7255802
andrew21-mch <nfonandrew73@gmail.com>
2022-01-04 00:52:07 +0100

    puppet

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,10 @@
+# Using Puppet, 
+file { '/tmp/school':
+  ensure  => present,
+  path    => '/tmp/school',
+  owner   => 'www-data',
+  group   => 'www-data',
+  mode    => '0744',
+  content => 'I love Puppet';
+  }
+ 

d3612d20f0c79c7d1364c6d7fcd6e8065c1d7b4e
andrew21-mch <nfonandrew73@gmail.com>
2022-01-05 04:55:31 +0100

    updated

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,14 @@
+# Connecting to server using puppet
+
+file_line { 'TurnOffPasswordAuthy':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => ' PasswordAuthentication no';
+  }
+
+file_line { 'DeclareIdentityFile':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => ' IdentityFile ~/.ssh/school';
+  }
+  

86dc3acb69b1c9a2bfe1199d6535cad9edca9eb1
nfon andrew <58951422+andrew21-mch@users.noreply.github.com>
2022-09-19 19:14:21 +0100

    Update 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,9 +1,7 @@
-#install puppet-lint
+#install flask
 
 package {
-  'flask': {
-    'ensure': 'present',
-    'provider': 'pip3',
-    'version': '2.1.0',
-  }
+  'flask':
+    ensure   => '2.1.0',
+    provider => 'pip3',
 }

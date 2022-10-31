ea1ff72b8dc8730f9357502daf37f51a501e6783
andrew21-mch <nfonandrew73@gmail.com>
2022-10-31 09:32:45 +0100

    clean

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,27 +1,27 @@
 #!/usr/bin/python3
-"""Exports data in the JSON format"""
+"""
+extend your Python script to export data in the JSON format
+"""
 
-if __name__ == "__main__":
+import json
+import requests
+from sys import argv
 
-    import json
-    import requests
-    import sys
-
-    users = requests.get("https://jsonplaceholder.typicode.com/users")
-    users = users.json()
-    todos = requests.get('https://jsonplaceholder.typicode.com/todos')
-    todos = todos.json()
-    todoAll = {}
-
+if __name__ == '__main__':
+    endpoint = "https://jsonplaceholder.typicode.com/"
+    data = {}
+    users = requests.get(endpoint + "users").json()
     for user in users:
-        taskList = []
-        for task in todos:
-            if task.get('userId') == user.get('id'):
-                taskDict = {"username": user.get('username'),
-                            "task": task.get('title'),
-                            "completed": task.get('completed')}
-                taskList.append(taskDict)
-        todoAll[user.get('id')] = taskList
+        records = []
+        todo = requests.get(endpoint + "todos?userId={}".
+                            format(user.get('id'))).json()
+        for task in todo:
+            records.append({
+                "username": user.get('username'),
+                "task": task.get('title'),
+                "completed": task.get('completed')
+            })
+        data["{}".format(user.get('id'))] = records
 
-    with open('todo_all_employees.json', mode='w') as f:
-        json.dump(todoAll, f)
+    with open("todo_all_employees.json", 'w') as f:
+        json.dump(data, f)

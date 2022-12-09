75696c4a89c1946c9ad1a008160b9fa5e8e2989f
andrew21-mch <nfonandrew73@gmail.com>
2022-12-09 23:24:21 +0100

    refactor api

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,29 +1,41 @@
 #!/usr/bin/python3
 """
-extend your Python script to export data in the JSON format
+Script that, using this REST API, for a given employee ID, returns
+information about his/her TODO list progress
+and export data in the JSON format.
 """
 
 import json
 import requests
 from sys import argv
 
-if __name__ == '__main__':
-    endpoint = "https://jsonplaceholder.typicode.com/"
-    userId = argv[1]
-    records = []
-    user = requests.get(endpoint + "users/{}".
-                        format(userId)).json()
-    todo = requests.get(endpoint + "todos?userId={}".
-                        format(userId)).json()
-    for task in todo:
-        records.append({
-            "task": task.get('title'),
-            "completed": task.get('completed'),
-            "username": user.get('username')
-        })
-    data = {
-        "{}".format(userId): records
-    }
 
-    with open("{}.json".format(userId), 'w') as f:
-        json.dump(data, f)
+if __name__ == "__main__":
+
+    sessionReq = requests.Session()
+
+    idEmp = argv[1]
+    idURL = 'https://jsonplaceholder.typicode.com/users/{}/todos'.format(idEmp)
+    nameURL = 'https://jsonplaceholder.typicode.com/users/{}'.format(idEmp)
+
+    employee = sessionReq.get(idURL)
+    employeeName = sessionReq.get(nameURL)
+
+    json_req = employee.json()
+    usr = employeeName.json()['username']
+
+    totalTasks = []
+    updateUser = {}
+
+    for all_Emp in json_req:
+        totalTasks.append(
+            {
+                "task": all_Emp.get('title'),
+                "completed": all_Emp.get('completed'),
+                "username": usr,
+            })
+    updateUser[idEmp] = totalTasks
+
+    file_Json = idEmp + ".json"
+    with open(file_Json, 'w') as f:
+        json.dump(updateUser, f)
